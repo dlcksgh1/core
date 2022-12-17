fac30644476f5b1185cc70d59b398e018d37c068
Lee  Chanho <sma05091@nate.com>
2022-12-17 22:46:37 +0900

    의존관계 자동 주입
    의존관계 자동 주입 - 다양한 의존관계 주입 방법
    의존관계 자동 주입 - 옵션 처리
    의존관계 자동 주입 - 생성자 주입을 선택해라!
    의존관계 자동 주입 - 롬복과 최신 트랜드
    의존관계 자동 주입 - 조회 빈이 2개 이상 - 문제
    의존관계 자동 주입 - @Autowired 필드 명, @Qualifier, @Primary
    의존관계 자동 주입 - 애노테이션 직접 만들기
    의존관계 자동 주입 - 조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,28 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
+
 public class OrderServiceImpl implements  OrderService{
 
     private final MemberRepository memberRepository;
-
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy ;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
